50b3d7a2c12989688ed328641b583de0ef6a7ce9
Tobias <tobrohl@gmail.com>
2021-09-15 08:29:37 +0200

    Documentation

--- src/de/theminefighter/Main.java
+++ src/de/theminefighter/Main.java
@@ -19,23 +19,25 @@ public class Main {
     public static void main(String[] args) throws Exception {
         if (args.length!=1)
             throw new Exception("A jnlp file must be provided to launch it");
-
+//load jnlp structure
         Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(args[0])).getDocumentElement();
         String codebase = root.getAttribute("codebase");
         Element resources = (Element) root.getElementsByTagName("resources").item(0);
         Element appDesc = (Element) root.getElementsByTagName("application-desc").item(0);
         String mainClassName = appDesc.getAttribute("main-class");
-
+//load server addresses and other stuff from jnlp to system properties
         NodeList props = resources.getElementsByTagName("property");
         for (int i = 0; i < props.getLength(); i++) {
             Element prop= (Element) props.item(i);
             System.setProperty(prop.getAttribute("name"),prop.getAttribute("value"));
         }
+        //load arguments for main method of sts
         NodeList argumentTags = appDesc.getElementsByTagName("argument");
         String[] launchArgs=new String[argumentTags.getLength()];
         for (int i = 0; i < argumentTags.getLength(); i++) {
             launchArgs[i]= argumentTags.item(i).getTextContent();
         }
+        //load sts code to system classpath
         NodeList jars = resources.getElementsByTagName("jar");
         URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
         Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
@@ -46,6 +48,7 @@ public class Main {
         }
         Class<?> loadedClass = sysloader.loadClass(mainClassName);
         Method mainMethod = loadedClass.getDeclaredMethod("main",String[].class);
+        //run sts
         mainMethod.invoke(null,new Object[]{launchArgs});
     }
 }
--- src/javax/jnlp/SingleInstanceListener.java
+++ src/javax/jnlp/SingleInstanceListener.java
@@ -1,4 +1,4 @@
 package javax.jnlp;
-
+//needed as the class loading procedure throws if this class is not present, because it's referenced hard by sts
 public interface SingleInstanceListener {
 }
