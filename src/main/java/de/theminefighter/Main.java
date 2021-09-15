3ce8517c955e25dcf5edf10ec331e93b872dbcd5
Tobias <tobrohl@gmail.com>
2021-09-15 23:21:44 +0200

    Now Java 8 and 11 supported

--- src/main/java/de/theminefighter/Main.java
+++ src/main/java/de/theminefighter/Main.java
@@ -19,6 +19,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         if (args.length!=1)
             throw new Exception("A jnlp file must be provided to launch it");
+        boolean oldJava=System.getProperty("java.version").startsWith("1.");
 //load jnlp structure
         Element root = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(new File(args[0])).getDocumentElement();
         String codebase = root.getAttribute("codebase");
@@ -38,9 +39,7 @@ public class Main {
             launchArgs[i]= argumentTags.item(i).getTextContent();
         }
         //load sts code to system classpath
-
-        ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
-
+        URLClassLoader finalClassLoader;
         NodeList jars = resources.getElementsByTagName("jar");
         URL[] urls= new URL[jars.getLength()];
         for (int i = 0; i < jars.getLength(); i++) {
@@ -48,8 +47,17 @@ public class Main {
             URL href = new URL(codebase + "/" + jar.getAttribute("href"));
             urls[i]=href;
         }
-        URLClassLoader ucl= URLClassLoader.newInstance(urls,systemClassLoader);
-        Class<?> loadedClass = ucl.loadClass(mainClassName);
+        if (oldJava) {
+            finalClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+            Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            method.setAccessible(true);
+            for (URL url : urls) {
+                method.invoke(finalClassLoader, url);
+            }
+        } else {
+            finalClassLoader= URLClassLoader.newInstance(urls, ClassLoader.getSystemClassLoader());}
+
+        Class<?> loadedClass = finalClassLoader.loadClass(mainClassName);
         Method mainMethod = loadedClass.getDeclaredMethod("main",String[].class);
         //run sts
         mainMethod.invoke(null,new Object[]{launchArgs});
